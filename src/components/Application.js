7aa32a3766869e5781ddff60e9ce85a36556af0c
Teddy Leung <teddy.ph.leung@gmail.com>
2019-11-26 15:14:40 -0800

    Installed axios and fetched days from the server on component mount.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import DayList from 'components/DayList';
 import Appointment from './Appointment';
 
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -71,6 +54,12 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+      .then(response => setDays(response.data));
+  }, []);
 
   return (
     <main className="layout">
