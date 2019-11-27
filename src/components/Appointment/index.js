e7f3e6ebde104f476c96000112f035ac3b0a0627
Teddy Leung <teddy.ph.leung@gmail.com>
2019-11-26 18:25:02 -0800

    With the useVisualMode custom hook we implemented the empty, show and create mode for appointment.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,20 +2,36 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "../../hooks/useVisualMode";
 
 import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 const Appointment = props => {  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
-      { props.interview ? 
-          <Show 
-            student={props.interview.student}
-            interviewer={props.interview.interviewer}
-          /> 
-          : <Empty /> 
-      }
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={back}
+        />
+      )}
     </article>
   );
 }
