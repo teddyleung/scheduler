9422bbb29acdc1ad3927af92706d739779e7bcc5
Teddy Leung <teddy.ph.leung@gmail.com>
2019-11-27 09:26:21 -0800

    Added Confirm action before Delete action.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 import useVisualMode from "../../hooks/useVisualMode";
 
 import "./styles.scss";
@@ -13,6 +14,7 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 const Appointment = props => {  
   const { mode, transition, back } = useVisualMode(
@@ -43,8 +45,7 @@ const Appointment = props => {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={deleteBooking}
-          appointmentId={props.id}
+          onDelete={() => transition(CONFIRM)}
         />
       )}
       {mode === CREATE && (
@@ -59,6 +60,13 @@ const Appointment = props => {
       )}
       {mode === DELETING && (
         <Status message="Deleting" />
+      )}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you would like to delete?"
+          onCancel={back}
+          onConfirm={() => deleteBooking(props.id)}
+        />
       )}
     </article>
   );
