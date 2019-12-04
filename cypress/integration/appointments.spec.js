8dee29a8bcda0cb066480e3d242b4b1fc5da959f
Teddy Leung <teddy.ph.leung@gmail.com>
2019-12-03 19:14:59 -0800

    Installed cypress and set up the config.

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,59 @@
+describe("Appointment", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Monday");
+  })
+  
+  it("should book an interview", () => {    
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+    
+    cy.get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones");
+    
+    cy.get("[alt='Sylvia Palmer']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {    
+    cy.contains("[data-testid=appointment]", "Archie Cohen")
+      .get("[alt=Edit]")
+      .click({ force: true });
+    
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Simon Schuster");
+    
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Simon Schuster");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.contains("[data-testid=appointment]", "Archie Cohen")
+    .get("[alt=Delete]")
+    .click({ force: true });
+
+    cy.contains("Confirm")
+      .click();
+
+    cy.contains("Deleting");
+    cy.contains("Deleting").should('not.exist');
+    
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should('not.exist');
+  })
+});
