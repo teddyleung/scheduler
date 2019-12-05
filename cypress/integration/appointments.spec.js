040bd5072a3c41d0c2bbde36654657d669e997be
Teddy Leung <teddy.ph.leung@gmail.com>
2019-12-04 16:59:15 -0800

    Added comments to Cypress tests.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,21 +1,26 @@
 describe("Appointment", () => {
   beforeEach(() => {
+    // Reset the database prior to each test
     cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
   })
   
   it("should book an interview", () => {    
+    // 1. Click the add image
     cy.get("[alt=Add]")
       .first()
       .click();
     
+    // 2. Type student name into the input field
     cy.get("[data-testid=student-name-input]")
       .type("Lydia Miller-Jones");
     
+    // 3. Select the mentor
     cy.get("[alt='Sylvia Palmer']")
       .click();
 
+    // 4. Click save
     cy.contains("Save")
       .click();
 
@@ -24,17 +29,21 @@ describe("Appointment", () => {
   });
 
   it("should edit an interview", () => {    
+    // 1. Click the Edit button
     cy.contains("[data-testid=appointment]", "Archie Cohen")
       .get("[alt=Edit]")
       .click({ force: true });
     
+    // 2. Enter student name
     cy.get("[data-testid=student-name-input]")
       .clear()
       .type("Simon Schuster");
     
+    // 3. Select the mentor  
     cy.get("[alt='Tori Malcolm']")
       .click();
 
+    // 4. Click save
     cy.contains("Save")
       .click();
 
@@ -43,16 +52,20 @@ describe("Appointment", () => {
   });
 
   it("should cancel an interview", () => {
+    // 1. Click Delete
     cy.contains("[data-testid=appointment]", "Archie Cohen")
-    .get("[alt=Delete]")
-    .click({ force: true });
+      .get("[alt=Delete]")
+      .click({ force: true });
 
+    // 2. Click confirm
     cy.contains("Confirm")
       .click();
 
+    // 3. The deleting loading page should show and go away
     cy.contains("Deleting");
     cy.contains("Deleting").should('not.exist');
     
+    // 4. The appointment should be deleted.
     cy.contains(".appointment__card--show", "Archie Cohen")
       .should('not.exist');
   })
